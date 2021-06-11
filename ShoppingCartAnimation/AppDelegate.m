a70886743103f1b5f73697b813460893f2512cc5
Ben <ben.c@spotec.net>
2021-06-11 17:10:37 +0800

    test

--- ShoppingCartAnimation/AppDelegate.m
+++ ShoppingCartAnimation/AppDelegate.m
@@ -14,7 +14,7 @@
 
 @implementation AppDelegate
 
-
+// test
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     return YES;
